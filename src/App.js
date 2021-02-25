bcfe84b918f00560003aeb7d2e9d93d9a69f769c
Nickolay Lyman <kolyalyman@gmail.com>
2021-02-25 18:27:02 +0200

    Выполнено дз

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   submitHandler = contactObj => {
     if (this.state.contacts.some(({ name }) => name === contactObj.name)) {
       return alert(`${contactObj.name} is already in contacts.`);
